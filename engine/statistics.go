bada1426c82f916bd7eecd9c3a874dc1d054c083
Weijie Zhao <maggch@outlook.com>
2022-11-24 09:04:08 +0800

    update

--- engine/statistics.go
+++ engine/statistics.go
@@ -1,14 +1,25 @@
 package engine
 
 type Statistics struct {
-	RoomCount int `json:"voiceRoomCount"`
+	RoomCount         int         `json:"voiceRoomCount"`
+	VoicePeerCount    int         `json:"voicePeerCount"`
+	VoicePeerCountMap map[int]int `json:"VoicePeerCountMap"`
 }
 
 func statistics(r *R) (*Statistics, error) {
 	rooms := r.router.engine.rooms
 	rooms.RLock()
 	defer rooms.RUnlock()
+	voicePeerCountMap := make(map[int]int)
+	voicePeerCount := 0
+	for _, v := range rooms.m {
+		peerCount := len(v.m)
+		voicePeerCount += peerCount
+		voicePeerCountMap[peerCount]++
+	}
 	return &Statistics{
-		RoomCount: len(rooms.m),
+		RoomCount:         len(rooms.m),
+		VoicePeerCount:    voicePeerCount,
+		VoicePeerCountMap: voicePeerCountMap,
 	}, nil
 }
